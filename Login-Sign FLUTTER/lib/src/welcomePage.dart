37e5e757b6985113543d5dfac5cf4f99e754568a
makarovpm <111125995+makarovpm@users.noreply.github.com>
2022-08-13 13:44:41 +0300

    LOGİN-SİGNUP FLUTTER

---
+++ Login-Sign FLUTTER/lib/src/welcomePage.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_signup/src/loginPage.dart';
+import 'package:flutter_login_signup/src/signup.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color.fromARGB(255, 10, 35, 177).withAlpha(100),
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Colors.white),
+        child: Text(
+          'Giris Yap',
+          style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _signUpButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SignUpPage()));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color.fromARGB(255, 10, 35, 177).withAlpha(100),
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Colors.white),
+        child: Text(
+          'Kayıt Ol',
+          style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
+        ),
+      ),
+    );
+  }
+
+  Widget _label() {
+    return Container(
+        margin: EdgeInsets.only(top: 40, bottom: 20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'TOUCH ID ile hızlı giris!',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Icon(Icons.fingerprint, size: 100, color: Colors.white),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Dokun Giris Yap!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ],
+        ));
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'TR',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.headline1,
+            fontSize: 50,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),
+          children: [
+            TextSpan(
+              text: 'ILI',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 243, 243, 243), fontSize: 50),
+            ),
+            TextSpan(
+              text: 'FE',
+              style: TextStyle(color: Colors.white, fontSize: 50),
+            ),
+          ]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.shade200,
+                    offset: Offset(2, 4),
+                    blurRadius: 5,
+                    spreadRadius: 2)
+              ],
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 10, 35, 177),
+                    Color.fromARGB(255, 100, 100, 100),
+                  ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _title(),
+              SizedBox(
+                height: 100,
+              ),
+              _submitButton(),
+              SizedBox(
+                height: 20,
+              ),
+              _signUpButton(),
+              SizedBox(
+                height: 20,
+              ),
+              _label()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
