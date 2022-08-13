d43da9905a8ff5f1f2262ecea1071c3e2fac53e6
makarovpm <111125995+makarovpm@users.noreply.github.com>
2022-08-13 13:48:02 +0300

    İntruction Flutter

---
+++ İntruction FLUTTER/lib/helpers/ColorsSys.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorSys {
+  static Color primary = Color.fromARGB(255, 255, 255, 255);
+  static Color gray = Color.fromARGB(255, 255, 255, 255);
+  static Color secoundry = Color.fromARGB(255, 255, 255, 255);
+}
---
+++ İntruction FLUTTER/lib/helpers/Strings.dart
@@ -0,0 +1,7 @@
+class Strings {
+  static var stepOneTitle = "TRILIFE";
+  static var stepOneContent =
+      "Artık hayatın her alanında ulaşabileceğin bir yer var.";
+  static var stepTwoTitle = "TRILIFE";
+  static var stepTwoContent = "";
+}
