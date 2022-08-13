5951007585127f2a0e9d9257264528e59992280d
makarovpm <111125995+makarovpm@users.noreply.github.com>
2022-08-13 14:52:26 +0300

    Delete İntruction FLUTTER directory

--- İntruction FLUTTER/lib/helpers/ColorsSys.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorSys {
-  static Color primary = Color.fromARGB(255, 255, 255, 255);
-  static Color gray = Color.fromARGB(255, 255, 255, 255);
-  static Color secoundry = Color.fromARGB(255, 255, 255, 255);
-}
--- İntruction FLUTTER/lib/helpers/Strings.dart
+++
@@ -1,7 +0,0 @@
-class Strings {
-  static var stepOneTitle = "TRILIFE";
-  static var stepOneContent =
-      "Artık hayatın her alanında ulaşabileceğin bir yer var.";
-  static var stepTwoTitle = "TRILIFE";
-  static var stepTwoContent = "";
-}
